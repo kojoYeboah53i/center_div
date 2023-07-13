8a76847d03f4a53c672a6f286e2cb17e94a88b10
Isaac Yeboah <yeboah.determined.isaac@gmail.com>
2023-07-13 12:22:03 +0100

    update

--- profile/main.js
+++ profile/main.js
@@ -6,3 +6,13 @@ const uploadInput = document.querySelector('.trick-upload input[type=file]');
 uploadButton.addEventListener('click', function() {
     uploadInput.click();
 });
+
+
+const readText = () => {
+
+    alert("hello I am ready to ready text")
+
+}
+
+
+// readText();
