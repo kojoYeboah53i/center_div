6bf51fb0144c1451a602489f8a4d31e6d73acddc
Isaac Yeboah <yeboah.determined.isaac@gmail.com>
2023-07-14 11:47:28 +0100

    update

--- profile/main.js
+++ profile/main.js
@@ -1,18 +1,54 @@
-// alert("javascript is working")
+
+// ------------------- wait for all element to load ------------------- 
+ $(document).ready(function() {
+
+
+console.log("main javascript file is working")
 
 const uploadButton = document.querySelector('div.img-profile div.iconDiv');
-const uploadInput = document.querySelector('.trick-upload input[type=file]');
 
 uploadButton.addEventListener('click', function() {
-    uploadInput.click();
+    document.querySelector('input[type=file]').click();
 });
 
 
-const readText = () => {
+$('.submit-update').click(function (e) { 
+    e.preventDefault();
+    
+    // ---------------------span  selecting elements from our webpage----------------------------
+   let student_name =  $('span#name');
+   let school = $('span#school');
+   let profile_email = $('span#email');
+   let profile_phone = $('span#contact');
 
-    alert("hello I am ready to ready text")
 
-}
+// ---------------------- get form data --------------------------
+   let first_name = $('input#firstname').val();
+   let last_name = $('input#lastname').val();
+    let school_name = $('input#school').val();
+    let email = $('input#email').val();
+    let phone = $('input#contact').val();
+
+    student_name.text('');
+    student_name.text(first_name+ " " +last_name);
+    school.text('');
+    school.text(school_name);
+    profile_email.text('');
+    profile_email.text(email);
+    profile_phone.text('');
+    profile_phone.text(phone);
 
 
-// readText();
+
+
+    
+});
+
+
+
+
+});
+
+// ------------------- wait for all element to load ends ------------------- 
+
+
