b13de20b82835d257887327d392a8b3e1686fae1
Isaac Yeboah <yeboah.determined.isaac@gmail.com>
2023-07-19 14:50:20 +0000

    new update

--- profile/main.js
+++ profile/main.js
@@ -26,14 +26,49 @@ uploadButton.addEventListener('click', function() {
 
 });
 
+setInterval(() => {
+
+const width = window.innerWidth;
+
+if( width <= 425){
+
+const contactLabel = document.querySelector("div.form-control.contact > label");
+$(contactLabel).text('');
+$(contactLabel).text('Contact');
+
+}
+
+}, 4000);
 
 
 
 
-    
+const changeProfile = document.querySelector('.header span');
+// -------- switch to edit profile---------------
+changeProfile.addEventListener('click', function() {
+    const editProfile = document.querySelector('.edit-profile');
+    const profileControl = document.querySelector('.profile-control');
+    profileControl.classList.remove('d-flex');
+    profileControl.classList.add('d-none');
+
+    // -----------  show edit profile --------------
+    editProfile.classList.remove('d-none');
+    editProfile.classList.add('d-block');
+
+    const editHeader = document.querySelector('.header h3');
+    editHeader.style.display = 'flex';
+});
+
+
+
+
 
 $('.submit-update').click(function (e) { 
     e.preventDefault();
+
+    const editHeader = document.querySelector('.header h3');
+    editHeader.style.display = 'none';
+
 
 
     // ---------------- get window width ----------------
@@ -42,13 +77,21 @@ $('.submit-update').click(function (e) {
 
     if( width <= 425){
         // alert("Please use a desktop to update your profile");
+
+      
         const editProfile = document.querySelector('.edit-profile');
         profileInfo = document.querySelector('.profile-control .profile-info');
-        editProfile.style.display = 'none';
+        // editProfile.style.display = 'none';
+        
         const profileControl = document.querySelector('.profile-control');
-        profileControl.style.height = '80vh';
-        profileInfo.style.display = 'block';
-        // $(profileControl).height('80vh'); 
+        profileControl.style.height = '100vh';
+        profileControl.classList.remove('d-none');
+        profileControl.classList.add('d-flex');
+    
+        // -----------  show edit profile --------------
+        editProfile.classList.remove('d-block');
+        editProfile.classList.add('d-none');
+
     }
     
     // ---------------------span  selecting elements from our webpage----------------------------
