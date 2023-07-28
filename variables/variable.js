783b79f892d2302ea4f2baea0b6726004006b289
Isaac Yeboah <yeboah.determined.isaac@gmail.com>
2023-07-28 14:43:33 +0000

    update

--- variables/variable.js
+++ variables/variable.js
@@ -12,5 +12,21 @@ window.addEventListener('load', () => {
         document.body.classList.toggle('theme-dark');
     });
 
+    setInterval(() => {
+    const width = window.innerWidth;
+    if(width <= 480) {
+        console.log('width is less than 480');
+        const sidebar = document.querySelector('.sidebar');
+        sidebar.classList.add('sidebar-short');
+    }
+    }, 4000);
 
+    const showBtn = document.querySelector('.show span');
+    const sidebar = document.querySelector('.sidebar');
+
+    showBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        sidebar.classList.remove('sidebar-short');
+        sidebar.classList.toggle('sidebar-full');
+    });
 });
